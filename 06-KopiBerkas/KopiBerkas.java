061829f29ae038a23a085996ea6b91eae311064e
nurul21 <auliaola310@yahoo.com>
2015-11-06 16:11:16 +0700

    no message

---
+++ 06-KopiBerkas/KopiBerkas.java
@@ -0,0 +1,36 @@
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class KopiBerkas{
+    public void kopi(String sumber, String sasaran) throws IOException {
+        FileInputStream masukan = null;
+        FileOutputStream keluaran = null;
+
+        // Deklarasi variabel
+        try {
+            // Object stream untuk masukkan
+            masukan = new FileInputStream(sumber);
+            keluaran = new FileOutputStream(sasaran);// ambil objek abestu bukak nama berkasnya kalaok ga ada dia throw excepytion 
+
+            // Coba baca  dari stream
+            int karakter = masukan.read();
+            // Selama masih ada data yang masih bisa dibaca
+            while (karakter != -1) {// kalau minus 1 dia selesai
+                // Lakukan sesuatu dengan data yang dibaca => Tampikan
+                keluaran.write(karakter);
+                // Coba baca lagi dari stream
+                karakter = masukan.read();
+            }
+            keluaran.flush();
+        } 
+        finally {
+            // Tutup stream masukan
+            if (masukan != null)
+                masukan.close();
+            if (keluaran != null)
+                keluaran.close();
+
+        }
+    }
+}
