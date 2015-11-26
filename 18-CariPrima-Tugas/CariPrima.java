e86cf84d8d5a30d9e467a1b0cad82c9d9082b488
nurul21 <auliaola310@yahoo.com>
2015-11-26 23:40:38 +0700

    no message

---
+++ 18-CariPrima-Tugas/CariPrima.java
@@ -0,0 +1,56 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CariPrima {
+    public static void main() throws IOException {
+        // Buat berkas untuk menulis hasil. Pakai WRITER karena yang ditulis 
+        // berkas text.
+        FileWriter berkas = new FileWriter(NAMA_BERKAS);
+
+        // Buat array dari thread
+        BenarPrima[] benarPrima = new BenarPrima[JUMLAH_THREAD];
+
+        Thread [] thread = new Thread[JUMLAH_THREAD];
+        // Mulai hitung dari angka 2, karena 1 otomatis bukan prima
+        int angka = 2;
+        // Loop sampai batas atas yang diminta
+        while (angka<=ANGKA_TERBESAR) {
+
+            for(int i=0; i<JUMLAH_THREAD;i++){           
+
+                benarPrima[i] = new BenarPrima(angka);             
+                thread[i] = new Thread(benarPrima[i]);     
+                angka++;
+
+            }
+
+            for (int k = 0; k < JUMLAH_THREAD; ++k)
+                thread[k].start();
+
+            for (int k = 0; k < JUMLAH_THREAD; ++k)
+                while (benarPrima[k].selesai() == false) { }
+
+            for(int i=0; i<JUMLAH_THREAD;i++){
+                if(benarPrima[i].selesai()){
+                    if(benarPrima[i].prima()){
+
+                        synchronized(berkas) {
+                            try {
+                                berkas.write(benarPrima[i].angka()+"\n");
+                            }
+                            catch (IOException kesalahan) {
+                                System.out.printf("Telah terjadi kesalahan: %s", kesalahan);
+                            }
+                        }
+
+                    }
+                }   
+            }
+        }
+
+        berkas.close();
+    }       
+    private final static String NAMA_BERKAS = "prima.log";
+    private final static int JUMLAH_THREAD = 10;
+    private final static int ANGKA_TERBESAR = 100000;
+}
